90c7c449640ca58c077d50996b3b762757b34822
JEONGHWANMIN <mimi0j0j@naver.com>
2023-11-06 21:03:02 +0900

    Feat : 캐쉬 타임 3분 추가

--- src/configs/configs.module.ts
+++ src/configs/configs.module.ts
@@ -2,6 +2,8 @@ import { Module } from "@nestjs/common";
 import { CacheModule } from "@nestjs/cache-manager";
 import { ConfigModule } from "@nestjs/config";
 
+const ONE_MINUTE = 60000;
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -13,7 +15,7 @@ import { ConfigModule } from "@nestjs/config";
     }),
     CacheModule.register({
       isGlobal: true,
-      ttl: 60000,
+      ttl: ONE_MINUTE * 3, // 60000 => 1분
       max: 100,
     }),
   ],
